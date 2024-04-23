e829f1d331dd9dc2887d2c2bd603e39b92cc149f
guillaume <guillaume.goubel.pro@gmail.com>
2024-04-23 16:36:19 +0200

    base template

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,17 @@
 import './bootstrap.js';
+// import './js/base/jquery.js';
+// import './js/base/vendors.min.js';
+import './js/base/main.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/base/vendors.min.css';
+import './styles/base/icon.min.css';
+import './styles/base/style.css';
+import './styles/base/responsive.css';
+import './styles/demos/restaurant/restaurant.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
--- importmap.php
+++ importmap.php
@@ -16,6 +16,10 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    'home' => [
+        'path' => './assets/js/pages/home.js',
+        'entrypoint' => true,
+    ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
