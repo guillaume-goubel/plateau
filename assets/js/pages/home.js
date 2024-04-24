7903d0f4e8a6ff52736b80c4e10d0c6da62edd42
guillaume <guillaume.goubel.pro@gmail.com>
2024-04-24 14:46:05 +0200

    front part 2

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -2,4 +2,21 @@ import '../../styles/pages/home.css';
 
 document.addEventListener("DOMContentLoaded", (event) => {
     
+    if (typeof $.fn.magnificPopup === 'function') {
+        if ($('#subscribe-popup').length > 0) {
+
+            $(document).on('click', '.popup-modal-menu', function (e) {
+                e.preventDefault();
+                $.magnificPopup.open({
+                    showCloseBtn: false,
+                    items: {
+                        src: '#subscribe-popup'
+                    },
+                    type: 'inline',
+                    mainClass: 'my-mfp-zoom-in'
+                });
+            });
+        }
+    }
+
 });
